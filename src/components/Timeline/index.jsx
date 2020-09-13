2afe4af8b3cf38d8e0c560aa45b69e64a28955bc
Karan Khanna <virtual.karankhanna@gmail.com>
2020-09-13 14:06:24 +0530

    add svg

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -14,8 +14,10 @@ const Heading = styled.h1`
 `;
 
 const CarouselSection = styled.div`
-  padding: 200px 0;
-  overflow-x: scroll;
+    min-height: 450px;
+    overflow: hidden;
+    padding-top: 30px;
+  overflow-x: auto;
   scroll-behavior: smooth;
   white-space: nowrap;
   scroll-snap-type: x mandatory;
@@ -49,6 +51,9 @@ text-align: center;
   transition: ${({ isFocused }) => (isFocused ? '.6s' : '.2s')}; 
   position: relative;
   transform: ${({ isFocused }) => (isFocused ? 'scale(3)' : 'initial')};
+  div {
+    display: inline-block;
+  }
 `;
 
 const ItemTarget = styled.div`
@@ -57,7 +62,6 @@ const ItemTarget = styled.div`
   position: absolute;
   top: calc(50% - 5px);
   left: calc(50% - 5px);
-  background-color: red;
   transform: scale(.1);
 `;
 
@@ -71,6 +75,21 @@ const NextBtn = styled.span`
 
 border: 1px solid white;
   color: white;
+`;
+
+/* const ItemBullet = styled.div`
+  position: absolute;
+  height: 20px;
+  width: 20px;
+  background-color: #F02484;
+  left: calc(50% - 10px);
+  bottom: -40px;
+  border-radius: 50%;
+  transform: ${({ isFocused }) => (isFocused ? 'scale(.334)' : 'initial')};
+`; */
+
+const TimelineCurve = styled.svg`
+margin-top:-100px;
 `;
 
 const Timeline = () => {
@@ -122,58 +141,84 @@ const Timeline = () => {
       <Heading>Heading</Heading>
       <CarouselSection id="scrollArea">
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 1} /> */}
           <ItemWrapper isFocused={activeId === 1}>
             <ItemTarget id="listItem1" isFocused={activeId === 1} />
+
           </ItemWrapper>
 
           {`1-${activeId === 1}`}
         </CarouselItem>
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 2} /> */}
           <ItemWrapper isFocused={activeId === 2}>
             {`2-${activeId === 2}`}
             <ItemTarget id="listItem2" isFocused={activeId === 2} />
+
           </ItemWrapper>
 
         </CarouselItem>
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 3} /> */}
           <ItemWrapper isFocused={activeId === 3}>
             {`3-${activeId === 3}`}
             <ItemTarget id="listItem3" isFocused={activeId === 3} />
+
           </ItemWrapper>
         </CarouselItem>
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 4} /> */}
           <ItemWrapper isFocused={activeId === 4}>
             {`4-${activeId === 4}`}
             <ItemTarget id="listItem4" isFocused={activeId === 4} />
+
           </ItemWrapper>
         </CarouselItem>
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 5} /> */}
           <ItemWrapper isFocused={activeId === 5}>
             {`5-${activeId === 5}`}
             <ItemTarget id="listItem5" isFocused={activeId === 5} />
+
           </ItemWrapper>
         </CarouselItem>
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 6} /> */}
           <ItemWrapper isFocused={activeId === 6}>
             {`6-${activeId === 6}`}
             <ItemTarget id="listItem6" isFocused={activeId === 6} />
+
           </ItemWrapper>
         </CarouselItem>
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 7} /> */}
           <ItemWrapper isFocused={activeId === 7}>
             {`7-${activeId === 7}`}
             <ItemTarget id="listItem7" isFocused={activeId === 7} />
+
           </ItemWrapper>
         </CarouselItem>
         <CarouselItem>
+          {/* <ItemBullet isFocused={activeId === 8} /> */}
           <ItemWrapper isFocused={activeId === 8}>
             {`8-${activeId === 8}`}
             <ItemTarget id="listItem8" isFocused={activeId === 8} />
+
           </ItemWrapper>
         </CarouselItem>
       </CarouselSection>
-      <PrevBtn onClick={() => { document.getElementById('scrollArea').scrollLeft = document.getElementById('scrollArea').scrollLeft - 515; }}>{'<< Previous'}</PrevBtn>
-      <NextBtn onClick={() => { document.getElementById('scrollArea').scrollLeft = document.getElementById('scrollArea').scrollLeft + 515; }}>{'Next >>'}</NextBtn>
+      <TimelineCurve width="100%" height="191" viewBox="0 0 1170 191" fill="none" xmlns="http://www.w3.org/2000/TimelineCurve">
+        <circle cx="1072" cy="56" r="18" fill="#070014" stroke="#F02484" strokeWidth="4" />
+        <circle cx="1072" cy="56" r="8" fill="#F02484" />
+        <circle cx="94" cy="68" r="18" fill="#070014" stroke="#F02484" strokeWidth="4" />
+        <circle cx="94" cy="68" r="8" fill="#F02484" />
+        <circle cx="583" cy="171" r="18" fill="#070014" stroke="#F02484" strokeWidth="4" />
+        <circle cx="583" cy="171" r="8" fill="#F02484" />
+        <path d="M1.5 20.0001C597 376 1168.5 2.50012 1168.5 2.50012" stroke="#F02484" strokeWidth="5" />
+      </TimelineCurve>
+
+      <PrevBtn onClick={() => { document.getElementById('scrollArea').scrollLeft = document.getElementById('scrollArea').scrollLeft - 450; }}>{'<< Previous'}</PrevBtn>
+      <NextBtn onClick={() => { document.getElementById('scrollArea').scrollLeft = document.getElementById('scrollArea').scrollLeft + 450; }}>{'Next >>'}</NextBtn>
 
     </Layout>
   );
